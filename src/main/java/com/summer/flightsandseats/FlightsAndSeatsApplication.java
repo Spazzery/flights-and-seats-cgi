d7c460d5011aea8d827fdb52ab21e8d711214081
aranto <aranto@ttu.ee>
2025-03-17 22:16:37 +0200

    init

---
+++ src/main/java/com/summer/flightsandseats/FlightsAndSeatsApplication.java
@@ -0,0 +1,13 @@
+package com.summer.flightsandseats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightsAndSeatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightsAndSeatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/summer/flightsandseats/FlightsAndSeatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summer.flightsandseats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightsAndSeatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
